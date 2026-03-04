db0390417f97d826b3e0d0c55a521ad4bce65998
jhony <jhonatansouzameza100@gmail.com>
2026-03-04 13:09:22 -0300

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,46 @@
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.setGlobalPrefix('api');
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1',
+  });
+
+  app.enableCors({
+    origin: '*',
+    credentials: true, // Permite cookies
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'x-api-key'],
+  });
+  
+  // Indica qué motor de parseo usar para interpretar los parámetros de las URLs (query params).
+  app.set('query parser', 'extended');
+
+  // Para usar dto con class-transformer y class-validator
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true,
+    transform: true
+  }));
+
+  const config = new DocumentBuilder()
+  .setTitle("ChillHop Studio API")
+  .setDescription("")
+  .setVersion("1.0")
+  .addBearerAuth()
+  .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("docs", app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
